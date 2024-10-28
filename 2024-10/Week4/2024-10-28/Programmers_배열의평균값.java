dc37c6c43dfa5b8ac3d0729cb7b2bd0547c2000a
Uk-jake <mag0225@naver.com>
2024-10-28 23:22:07 +0900

    Add Programmers 배열의평균값

--- 2024-10/Week4/2024-10-28/Programmers_배열의평균값.java
+++ 2024-10/Week4/2024-10-28/Programmers_배열의평균값.java
@@ -1,4 +1,4 @@
-public class Programmers_피자나눠먹기2 {
+public class Programmers_배열의평균값 {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
