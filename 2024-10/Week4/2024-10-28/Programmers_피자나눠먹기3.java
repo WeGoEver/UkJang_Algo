5ceb0855de5a99f833d7c13f80fd012cff4a8fee
Uk-jake <mag0225@naver.com>
2024-10-28 23:17:00 +0900

    Add Programmers 피자 나눠먹기3

---
+++ 2024-10/Week4/2024-10-28/Programmers_피자나눠먹기3.java
@@ -0,0 +1,24 @@
+public class Programmers_피자나눠먹기2 {
+    public static void main(String[] args){
+
+        Solution solution = new Solution();
+
+        System.out.print(solution.solution(7, 10));
+    }
+}
+
+class Solution{
+    public int solution(int slice, int n) {
+        int answer = 0;
+
+        int tmp = n % slice;
+
+        if(tmp == 0){
+            answer = n / slice;
+        } else{
+            answer = n / slice + 1;
+        }
+
+        return answer;
+    }
+}
