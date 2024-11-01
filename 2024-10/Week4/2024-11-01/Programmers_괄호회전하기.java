3a897fcb6fc990da05c8495cca67d8888c6ff898
UkJang <mag0225@naver.com>
2024-11-01 11:57:28 +0900

    Add Programmers 괄호회전하기

---
+++ 2024-10/Week4/2024-11-01/Programmers_괄호회전하기.java
@@ -0,0 +1,69 @@
+import java.util.Stack;
+
+public class Programmers_괄호회전하기 {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String input = "[({})]";
+
+        System.out.println(solution.solution(input));
+    }
+}
+
+class Solution {
+    public int solution(String input){
+
+        String rolling = input;
+
+        int count = 0;
+
+        for(int i = 0; i < rolling.length(); i++){
+
+            if(isValid(rolling)){
+                count++;
+            }
+
+            // 문자열 회전
+            rolling = rolling.substring(1)+rolling.charAt(0);
+
+        }
+
+        return count;
+    }
+
+    public boolean isValid(String string) {
+
+        char[] brackets = string.toCharArray();
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < brackets.length; i++){
+
+            char tmp = brackets[i];
+
+            if( tmp == '[' || tmp == '{' || tmp == '('){
+                stack.add(tmp);
+            }
+            else if(tmp == ']'){
+                if(stack.isEmpty() || stack.peek() != '['){
+                    return false;
+                }
+                stack.pop();
+            }
+            else if(tmp == '}'){
+                if(stack.isEmpty() || stack.peek() != '{'){
+                    return false;
+                }
+                stack.pop();
+            }
+            else if(tmp == ')'){
+                if(stack.isEmpty() || stack.peek() != '('){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
