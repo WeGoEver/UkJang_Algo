0075c8f4234c8bf7318b4036f8ef76f57a9fa67c
UkJang <mag0225@naver.com>
2024-11-04 09:26:22 +0900

    Add Programmers 짝지어젝하기

---
+++ 2024-11/Week1/Programmers_짝지어제거하기.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+
+public class Programmers_짝지어제거하기{
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution("cc"));
+
+    }
+
+    // Solution 클래스를 정적(static)으로 변경
+    private static class Solution{
+        public int solution(String str){
+
+            // ArrayDeque 인터페이스로 Stack 선언
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+
+            // for 문을 이용해서 짝이 있는지 확인
+            for(int i = 0; i < str.length(); i++){
+                char tmp = str.charAt(i);
+
+                if(stack.isEmpty() || stack.peekLast() != tmp){
+                    stack.addLast(tmp);
+                }else if(stack.peekLast() == tmp){
+                    stack.pollLast();
+                }
+            }
+
+            int answer = 0;
+
+            if(stack.isEmpty()){
+                answer = 1;
+            }
+
+            return answer;
+        }
+    }
+}
