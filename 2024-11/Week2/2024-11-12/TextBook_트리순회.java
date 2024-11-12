ba95101e3106c03c3cbc1f77f02ee749615f9c44
UkJang <mag0225@naver.com>
2024-11-12 17:44:58 +0900

    init 트리순회

---
+++ 2024-11/Week2/2024-11-12/TextBook_트리순회.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TextBook_트리순회 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] input = {1,2,3,4,5,6,7};
+
+        System.out.println(Arrays.toString(solution.solution(input)));
+
+    }
+
+    private static class Solution {
+
+        public String[] solution(int[] arr) {
+        
+            
+            String[] answer = {"1","1"};
+
+            return answer;
+        }
+    }
+}
