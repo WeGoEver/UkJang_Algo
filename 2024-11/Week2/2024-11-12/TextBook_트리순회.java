067387179468781d9f7a3fbd3191e4881dc0ffb1
UkJang <mag0225@naver.com>
2024-11-12 17:45:13 +0900

    init 트리순회

--- 2024-11/Week2/2024-11-12/TextBook_트리순회.java
+++ 2024-11/Week2/2024-11-12/TextBook_트리순회.java
@@ -6,7 +6,7 @@ public class TextBook_트리순회 {
 
         Solution solution = new Solution();
 
-        int[] input = {1,2,3,4,5,6,7};
+        int[] input = { 1, 2, 3, 4, 5, 6, 7 };
 
         System.out.println(Arrays.toString(solution.solution(input)));
 
@@ -15,11 +15,29 @@ public class TextBook_트리순회 {
     private static class Solution {
 
         public String[] solution(int[] arr) {
+
+            // 입력 받은 정수 배열 이진 트리로 변환
+            int[]  binaryTree = new int[arr.length + 1];
+
+            // 자신의 자리 찾아서 삽입
+            int index = 1;
+
+           for(int tmp : arr){
+                if(binaryTree[index] == 0){
+                    binaryTree[index] = tmp;
+                }
+
+           }
         
             
             String[] answer = {"1","1"};
 
             return answer;
         }
+
+        public static void insertTree(int[] tree, int number){
+            
+        }
+
     }
 }
